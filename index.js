4fdc6214bf10862f1a87cfcb734f186137d284f0
HP <martinsonyedikachi@gmail.com>
2021-10-04 16:40:00 +0100

    MongoDB setup completed, data displayed

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 
 const app = require('./server')
-
 const port = process.env.PORT || 4001;
-const JWT_SECRET = process.env.JWT;
-const MONGODB_URL = process.env.MONGODB
 
-mongoose.connect(MONGODB_URL, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
-const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: false },
-    password: { type: String, required: true }
-}, { collection: 'users' })
-
-const User = mongoose.model('User', UserSchema);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const cookiepee = require('cookie-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 
 const server = express()
@@ -9,13 +7,82 @@ const server = express()
 server.set('view engine', 'ejs')
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
-server.use(cookiepee());
 server.use(express.static('public'));
+
+// ============ Using MongoClient =========================
+
+const mongoUrl = 'mongodb://127.0.0.1:27017';
+let db
+
+MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+    .then(client => {
+        console.log(`connected mongoDB: ${mongoUrl}`)
+        const db = client.db("newTestBase")
+        const quotesCollection = db.collection('quotes')
+
+        server.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/browse')
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+
+        server.get('/browsemongo', (req, res) => {
+            db.collection('quotes').find().toArray()
+                .then(result => {
+                    res.render('index.ejs', { quotes: result })
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+// ========================== Using Mongoose ============================
+
+//const mongoose = require('mongoose');
+// const mongoUrl = 'mongodb://127.0.0.1:27017/test'
+
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// let db = mongoose.connection
+// db.once('open', _ => {
+//     console.log("Databe connection: ", mongoUrl)
+// })
+// db.on('error', err => {
+//     console.error('connection error: ', err)
+// })
 
 
 server.get('/', (req, res) => {
     res.json({ message: "Welcome to the default zone, please specify a path" })
 })
 
+server.get('/browse', (req, res) => {
+    res.sendFile(__dirname + '/browserRoute.html')
+})
+
+// server.get('/browsemongo', (req, res) => {
+//     let gotten = db.collection('quotes').find()
+//     console.log(gotten)
+// })
+
+// server.post('/quotes', (req, res) => {
+//     quotesCollection.insertOne(req.body)
+//         .then(result => {
+//             console.log(result)
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+// })
 
 module.exports = server;
